4124cffe44e94f7aa6e0ea2528e8b995dd5594d0
xiaoming <m13750434238_1@163.com>
2018-08-07 02:11:59 +0800

    增加视频演示模块，完成简单编程题-1的输入合法性检查函数

--- static/js/get_calculate_result_function.js
+++ static/js/get_calculate_result_function.js
@@ -1,15 +1,32 @@
 function check_input_2(inputStr) {
-    return true;
+    var inputArray = inputStr.split(',');
+    var count = 0;
+    for (var i = 0; i < inputArray.length; i++) {
+        if ((!isNaN(inputArray[i]) || inputArray[i] === ',') && inputArray[i] !== ' ') {
+            count++;
+        }
+    }
+    alert('hh' + count + 'chang' + inputArray.length);
+
+    if (count === inputArray.length) {
+        return true;
+    } else {
+        return false;
+    }  
 }
 
 function get_result() {
     var inputStr = document.getElementById("inputStr").value;
-    var inputArray = inputStr.split(",").map(function char2num(c) { return Number(c); });
-    var sequence = new Sequence(inputArray);
-    document.getElementById("min").innerHTML = " o) 最小值 = " + sequence.minimum();
-    document.getElementById("max").innerHTML = " o) 最大值 = " + sequence.maximun();
-    document.getElementById("nums").innerHTML = " o) 元素数量 = " + sequence.get_length();
-    document.getElementById("average").innerHTML = " o) 平均值 = " + sequence.average();
+    if (check_input_2(inputStr)) {
+        var inputArray = inputStr.split(",").map(function char2num(c) { return Number(c); });
+        var sequence = new Sequence(inputArray);
+        document.getElementById("min").innerHTML = " o) 最小值 = " + sequence.minimum();
+        document.getElementById("max").innerHTML = " o) 最大值 = " + sequence.maximun();
+        document.getElementById("nums").innerHTML = " o) 元素数量 = " + sequence.get_length();
+        document.getElementById("average").innerHTML = " o) 平均值 = " + sequence.average();
+    } else {
+        document.getElementById("min").innerHTML = "输入数据格式错误，请重新输入！";
+    }
 }
 
 class Sequence {
--- static/js/get_remainder_function.js
+++ static/js/get_remainder_function.js
@@ -1,27 +1,35 @@
 // 定义错误类型
-const ISNAN = 0;
-const ISNULL = 1;
-const ISZERO = 2;
-const ISOK = 3;
+const ISNULL = 0;        // 没有输入，eg：''
+const ISNAN = 1;         // 输入不是数字，eg：'十度非ddsdqq.,./;'
+const ISSPACE = 2;       // 输入含有空格符，eg：'123 345 5 67'
+const ISZERO = 3;        // 第二个数不能为0，eg：2 % 0，除数不能为0
+const ISILLEGALNUM = 4;  // 输入为非法数字，eg：'0000001', '03', '0004'
+const ISOK = 5;          // 输入合法（允许负数、小数情况存在）
 // 求余
 function get_remainder() {
     var num1 = document.getElementById("num1").value;
     var num2 = document.getElementById("num2").value;
     switch (check_input_1(num1, num2)) {
-        case ISNAN: document.getElementById("result").innerHTML = "注: 请确保输入为数字类型！"; break;
-        case ISNULL: document.getElementById("result").innerHTML = "注: 请确保输入不含空字符！"; break;
-        case ISZERO: document.getElementById("result").innerHTML = "注: 第二个数不能为0！"; break;
+        case ISNULL: alert("错误：输入不能为空！"); break;
+        case ISNAN: alert("错误：输入不能为其它字符类型！"); break;
+        case ISSPACE: alert("错误：输入不能含有空格符！"); break;
+        case ISZERO: alert("错误：第二个数不能为0！"); break;
+        case ISILLEGALNUM: alert("错误：输入为非法数字，eg：'0000001', '03', '0004'！"); break;
         case ISOK: document.getElementById("result").innerHTML = (num1 % num2).toString(); break;
     }
 }
 // 输入检查
 function check_input_1(num1, num2) {
-    if (isNaN(num1) || isNaN(num2)) {    // 输入不是数字
-        return ISNAN;
-    } else if (!(/^[0-9]+.?[0-9]*/).test(num1) || !(/^[0-9]+.?[0-9]*/).test(num2)) {    // 输入为空
+    if (num1 === '' || num2 === '') {    // 输入为空
         return ISNULL;
+    } else if (isNaN(num1) || isNaN(num2)) {    // 输入不是数字
+        return ISNAN;
+    } else if (!(/^[0-9]+.?[0-9]*/).test(num1) || !(/^[0-9]+.?[0-9]*/).test(num2)) {    // 输入含有空格
+        return ISSPACE;
     } else if (Number(num2) === 0) {    // 除数为0
         return ISZERO;
+    } else if (ISILLEGALNUM) {          // 输入为非法数字
+        return ISILLEGALNUM;
     } else {                            // 输入正确
         return ISOK;
     }
